904b0273463b86faedf4119dabd0d5af3f5496de
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2021-08-05 17:15:18 -0800

    time

--- src/Component/CountDown.jsx
+++ src/Component/CountDown.jsx
@@ -1,7 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component,useState,useRef,useEffect } from 'react';
 import '../App.css';
 
+
+
+
 function CountDown() {
+    // ------hooks------
+    let interval=useRef();
+    const [day,setDay]=useState('00');
+    const [hour,setHour]=useState('00');
+    const [minute,setMinute]=useState('00');
+    const [second,setSecond]=useState('00');
+
+    const startTime=()=>{
+        const newYear=new Date('December 31,2021 23:59:59').getTime();
+        // December 31,2021 23:59:59
+
+        interval=setInterval(()=>{
+            const today=new Date().getTime();
+            const distance=newYear-today;
+            /*
+            Time calculations: https://stackoverflow.com/questions/10804042/calculate-time-difference-with-javascript
+            */
+
+            const days=Math.floor(distance/(1000*60*60*24));
+            const hours=Math.floor((distance%(1000*60*60*24)/(1000*60*60)));
+            const minutes=Math.floor((distance%(1000*60*60))/(1000*60));
+            const seconds=Math.floor((distance %(1000*60))/1000);
+
+            if(distance<0){
+                clearInterval(interval.current);
+            }else{
+                setDay(days);
+                setHour(hours);
+                setMinute(minutes);
+                setSecond(seconds);
+            }
+
+
+        },1000);
+    }
+    
+    useEffect(()=>{
+        startTime();
+        return ()=>{
+            clearInterval(interval.current);
+        };
+    });
+
   return (
     <div className="card">
         <div className="header">
@@ -16,7 +62,7 @@ function CountDown() {
                     <span></span>
                     <span></span>
                     <span></span>
-                    <span className="time">100</span>
+                    <span className="time">{day}</span>
                 </div>
                 <div className="timetype">
                     <small>Days</small>
@@ -29,7 +75,7 @@ function CountDown() {
                     <span></span>
                     <span></span>
                     <span></span>
-                    <span className="time">17</span>
+                    <span className="time">{hour}</span>
                 </div>
                 <div className="timetype">
                     <small>hours</small>
@@ -44,7 +90,7 @@ function CountDown() {
                     <span></span>
                     <span></span>
                     <span></span>
-                    <span className="time">39</span>
+                    <span className="time">{minute}</span>
                 </div>
                 <div className="timetype">
                     <small>minutes</small>
@@ -59,7 +105,7 @@ function CountDown() {
                     <span></span>
                     <span></span>
                     <span></span>
-                    <span className="time">24</span>
+                    <span className="time">{second}</span>
                 </div>
                 <div className="timetype">
                     <small>Seconds</small>
